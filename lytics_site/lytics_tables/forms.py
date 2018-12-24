fc9864a53550a53abe36c955b9aaa3ba921e4450
Jonathan <jherr029@ucr.edu>
2018-12-24 11:47:36 -0800

    added error handlers

--- lytics_site/lytics_tables/forms.py
+++ lytics_site/lytics_tables/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from django.forms import formset_factory
 
 class ChannelForm(forms.Form):
     the_channel = forms.CharField(label='channel', max_length=30)
 
 class CustomQueryForm(forms.Form):
     custom_query = forms.CharField(label='sql', max_length=200)
+
+class AddressForm(forms.Form):
+    name = forms.CharField(
+        label='Address Please',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Address address here please'
+        })
+    )
+
+AddressFormset = formset_factory(AddressForm, extra=1)
